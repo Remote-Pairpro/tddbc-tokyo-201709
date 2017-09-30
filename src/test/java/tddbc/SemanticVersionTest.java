a805d18b518909a320add32331c2a71524514662
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-09-30 14:00:46 +0900

    メソッドリネーム。

--- src/test/java/tddbc/SemanticVersionTest.java
+++ src/test/java/tddbc/SemanticVersionTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class SemanticVersionTest {
 
     @Test
-    public void バージョン番号を与えれば文字列表現を生成することが出来る() {
+    public void バージョン番号に1_4_2を与えれば文字列表現を生成することが出来る() {
         SemanticVersion sut = new SemanticVersion(1, 4, 2);
         String actual = sut.toText();
         assertThat(actual, is("1.4.2"));
